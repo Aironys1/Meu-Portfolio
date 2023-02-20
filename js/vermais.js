7c58f049add65c0d03a0842d877345ab9044793b
Aironys1 <aironys1@hotmail.com>
2023-02-20 15:53:11 -0300

    Leia mais

--- js/vermais.js
+++ js/vermais.js
@@ -14,3 +14,21 @@ function leiaMais(){
         btnLeiaMais.innerHTML="Leia Menos";
     }
 }
+
+
+function leiaMaisMusica(){
+    let pontos = document.getElementById("pontosmusica");
+    let maisTexto=document.getElementById("maismusica");
+    let btnLeiaMais=document.getElementById("btnLeiaMaisMusica");
+
+    if(pontos.style.display==="none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="Leia Mais";
+
+    } else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos";
+    }
+}
