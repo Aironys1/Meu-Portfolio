5e0e826e9056b8f95010fbf548f841a35023fee9
Aironys1 <aironys1@hotmail.com>
2023-02-20 15:37:51 -0300

    Incluinfo JS

---
+++ js/vermais.js
@@ -0,0 +1,16 @@
+function leiaMais(){
+    let pontos = document.getElementById("pontos");
+    let maisTexto=document.getElementById("mais");
+    let btnLeiaMais=document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display==="none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="Leia Mais";
+
+    } else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos";
+    }
+}
